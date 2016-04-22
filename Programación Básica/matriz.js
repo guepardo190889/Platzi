6d6196a5b09f4d33ec1418135dce385ad91b28cc
guepardo190889 <guepardo190889@gmail.com>
2016-04-22 13:58:42 -0500

    matrices

---
+++ Programación Básica/matriz.js
@@ -0,0 +1,21 @@
+//Arrays, pueden ser de 2 tipos:
+//El Vector tiene una dimensión
+//La matriz tiene dos dimensiones
+//Las "coordenadas" en una matriz son: primero las columnas de arriba a abajo y luego las filas de izquierda a derecha
+
+//var menu = ["Productos", "Ventas", "Contacto"];
+//document.write(menu[2]);
+
+//var dofa = [ ["Fuerza", "Oportunidad"] , ["Debilidades", "Amenazas"] ];
+//document.write(dofa[1][1]);
+
+explosion();
+
+function explosion() {
+	alert("BOOOOOOOM!!!");
+	document.write("<h1>Elegiste un área minada :( </h1>");
+}
+
+function ganaste() {
+	document.write("Eres un ganador :)");
+}
