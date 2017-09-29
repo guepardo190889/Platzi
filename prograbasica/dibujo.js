9bfa69a644408bd9b4abe39a7f7f238f5c7d2499
guepardo190889 <guepardo190889@gmail.com>
2017-09-29 00:16:09 -0500

    Aniadir ingreso de numero de lineas y posicion de dibujo desde cajas de texto con eventos que escuchan a un boton de enviar. Tambien se aniadio validacion de letras no permitidas en las cajas de texto

--- prograbasica/dibujo.js
+++ prograbasica/dibujo.js
@@ -5,67 +5,78 @@ var SUPERIOR_DERECHA = 2;
 var INFERIOR_IZQUIERDA = 3;
 var INFERIOR_DERECHA = 4;
 
+var t_posicion = document.getElementById("it_posicion");
+var t_lineas = document.getElementById("it_lineas");
+var b_enviar = document.getElementById("ib_enviar");
+b_enviar.addEventListener("click", click_dibujar);
+var b_limpiar = document.getElementById("ib_limpiar");
+b_limpiar.addEventListener("click", click_limpiar);
+
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var ancho = d.width;
+var alto = d.height;
 
-var posicion = parseInt(prompt("Dónde quieres dibujar? \n 1=superior-izquierda \n 2=superior-derecha \n 3=inferiror-izquierda \n 4=inferior-derecha"));
-var intervalo = parseInt(prompt("Cuántos pixeles de separación debe haber entre líneas? (1-20)"));
-//console.log("intervalo: " + intervalo);
-var numLineas = parseInt(500/intervalo);
-//console.log("numLineas: " + numLineas);
-var xInicial,yInicial,xFinal,yFinal;
+var posicion, numLineas, intervalo, valido;
 
-if(intervalo > 0 && intervalo <= 20) {
-	if(SUPERIOR_IZQUIERDA == posicion) {
-		yInicial = 500
-		xFinal = 0;
-		for(i = 0; i < numLineas; i++) {
-			yInicial = yInicial - intervalo;
-			xFinal = xFinal + intervalo;
-			dibujaLinea("red", 0, yInicial, xFinal, 0);
-		}
-		dibujaLinea("black", 1, 1, 499, 1);
-		dibujaLinea("black", 1, 1, 1, 499);
-	}
-	else if(SUPERIOR_DERECHA == posicion) {
-		yInicial = 500;
-		xFinal = 500;
-		for(i = 0; i < numLineas; i++) {
-			yInicial = yInicial - intervalo;
-			xFinal = xFinal - intervalo;
-			dibujaLinea("red", 500, yInicial, xFinal, 0);
-		}
-		dibujaLinea("black", 1, 1, 499, 1);
-		dibujaLinea("black", 499, 499, 499, 1);
-	}
-	else if(INFERIOR_IZQUIERDA == posicion) {
-		yInicial = 0;
-		xFinal = 0;
-		for(i = 0; i < numLineas; i++) {
-			yInicial = yInicial + intervalo;
-			xFinal = xFinal + intervalo;
-			dibujaLinea("red", 0, yInicial, xFinal, 500);
-		}
-		dibujaLinea("black", 1, 1, 1, 499);
-		dibujaLinea("black", 1, 499, 499, 499);
-	}
-	else if(INFERIOR_DERECHA == posicion) {
-		yInicial = 0;
-		xFinal = 500
-		for(i = 0; i < numLineas; i++) {
-			yInicial = yInicial + intervalo;
-			xFinal = xFinal - intervalo;
-			dibujaLinea("red", 500, yInicial, xFinal, 500);
-		}
-		dibujaLinea("black", 499, 1, 499, 499);
-		dibujaLinea("black", 1, 499, 499, 499);
-	}
-	else {
-		alert("Estás tonto. Aprende a seguir instrucciones. La posición es de 1 a 4");
-	}
+dibujaRectangulo();
+
+function click_dibujar() {
+	posicion = parseInt(t_posicion.value);
+	numLineas = parseInt(t_lineas.value);
+	intervalo = parseInt(ancho / numLineas);
+
+	algoritmo();
 }
-else {
-	alert("Hombre!!! Los pixeles son de 1 a 20!!!");
+
+function click_limpiar() {
+	lienzo.clearRect(0,0,ancho,alto);
+	dibujaRectangulo();
+}
+
+function algoritmo() {
+	var xInicial ,yInicial, xFinal, yFinal;
+
+	validar();
+
+	if(valido == 1) {
+		if(SUPERIOR_IZQUIERDA == posicion) {
+			yInicial = alto
+			xFinal = 0;
+			for(i = 0; i < numLineas; i++) {
+				yInicial = yInicial - intervalo;
+				xFinal = xFinal + intervalo;
+				dibujaLinea("red", 0, yInicial, xFinal, 0);
+			}
+		}
+		else if(SUPERIOR_DERECHA == posicion) {
+			yInicial = alto;
+			xFinal = ancho;
+			for(i = 0; i < numLineas; i++) {
+				yInicial = yInicial - intervalo;
+				xFinal = xFinal - intervalo;
+				dibujaLinea("red", ancho, yInicial, xFinal, 0);
+			}
+		}
+		else if(INFERIOR_IZQUIERDA == posicion) {
+			yInicial = 0;
+			xFinal = 0;
+			for(i = 0; i < numLineas; i++) {
+				yInicial = yInicial + intervalo;
+				xFinal = xFinal + intervalo;
+				dibujaLinea("red", 0, yInicial, xFinal, alto);
+			}
+		}
+		else if(INFERIOR_DERECHA == posicion) {
+			yInicial = 0;
+			xFinal = ancho
+			for(i = 0; i < numLineas; i++) {
+				yInicial = yInicial + intervalo;
+				xFinal = xFinal - intervalo;
+				dibujaLinea("red", ancho, yInicial, xFinal, alto);
+			}
+		}
+	}
 }
 
 function dibujaLinea(c, xi, yi, xf, yf){
@@ -76,4 +87,38 @@ function dibujaLinea(c, xi, yi, xf, yf){
 	lienzo.lineTo(xf,yf);
 	lienzo.stroke();
 	lienzo.closePath();
+}
+
+function dibujaRectangulo() {
+	dibujaLinea("black", 1, 1, ancho - 1, 1);
+	dibujaLinea("black", ancho - 1, 1, ancho - 1, alto - 1);
+	dibujaLinea("black", ancho - 1, alto - 1, 1, alto - 1);
+	dibujaLinea("black", 1, alto - 1, 1, 1);
+}
+
+function validar() {
+	valido = 1;
+
+	if(valido == 1 && isNaN(posicion)) {
+		valido = 0;
+		alert("La posición sólo admite números, no letras");
+	}
+
+	if(valido == 1 && (SUPERIOR_DERECHA != posicion && 
+		SUPERIOR_IZQUIERDA != posicion && 
+		INFERIOR_IZQUIERDA != posicion && 
+		INFERIOR_DERECHA != posicion)) {
+		valido = 0;
+		alert("La posición debe ser de 1 a 4");
+	}
+	
+	if(valido == 1 && isNaN(numLineas)) {
+		valido = 0;
+		alert("La cantidad de líneas sólo admite números, no letras");
+	}
+
+	if(valido == 1 && numLineas > ancho) {
+		valido = 0;
+		alert("El número máximo de líneas es de " + ancho);
+	}
 }
