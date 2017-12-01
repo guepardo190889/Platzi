3c700bc28011cf804580c79ae37df0c1432f12cc
guepardo190889 <sluis@facturable.com>
2017-11-30 18:26:48 -0600

    crear objeto de telcas

--- prograbasica/teclas/eventos.js
+++ prograbasica/teclas/eventos.js
@@ -1,6 +1,11 @@
 
+var teclas = {UP:38,DOWN:40,LEFT:37,RIGTH:39};
+
 document.addEventListener("keyup", dibujarTeclado);
 
 function dibujarTeclado(event) {
-  console.log(event);
+  if(teclas.UP == event.keyCode) {
+    console.log("TECLA ARRIBA");  
+  }
+
 }
