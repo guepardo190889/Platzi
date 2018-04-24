d5f58c9e3e4f82032c24ed5773bbe1abdfc83985
guepardo190889 <guepardo190889@gmail.com>
2018-04-23 23:44:03 -0500

    Primera forma de dibujar en canvas vaca, cerdo, pollo, pero sin orden aún. En este caso, a veces parece que no se dibujan todos los animales porque lo que sucedió es que los animales que no se dibujaron supuestamente se dibujaron antes que el tile y este, como canvas siempre dibuja encima pues los tapa

--- prograbasica/villaplatzi/platzi.js
+++ prograbasica/villaplatzi/platzi.js
@@ -1,13 +1,40 @@
-var TILE = "tile.png";
+var MAPA = "tile.png";
+var VACA = "vaca.png";
+var CERDO = "cerdo.png";
+var POLLO = "pollo.png";
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 
-var imagen = new Image();
-imagen.src = TILE;
-imagen.addEventListener("load", dibujar);
+var fondo = new Image();
+fondo.src = MAPA;
+fondo.addEventListener("load", dibujarFondo);
 
-function dibujar() {
-  papel.drawImage(imagen, 100, 100);
+var vaca = new Image();
+vaca.src = VACA;
+vaca.addEventListener("load", dibujarVacas);
+
+var cerdo = new Image();
+cerdo.src = CERDO;
+cerdo.addEventListener("load", dibujarCerdos);
+
+var pollo = new Image();
+pollo.src = POLLO;
+pollo.addEventListener("load", dibujarPollos);
+
+function dibujarFondo() {
+  papel.drawImage(fondo, 0, 0);
+}
+
+function dibujarVacas() {
+  papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarCerdos() {
+  papel.drawImage(cerdo, 100, 300);
+}
+
+function dibujarPollos() {
+  papel.drawImage(pollo, 300, 150);
 }
 
 function aleatorio(min, max) {
