0ce71582e039fdff1f87dae5d1e54b829981ec5c
guepardo190889 <guepardo190889@gmail.com>
2018-04-24 00:16:53 -0500

    dibujar 10 vacas en posiciones aleatorias dentro del grid

--- prograbasica/villaplatzi/platzi.js
+++ prograbasica/villaplatzi/platzi.js
@@ -2,39 +2,67 @@ var MAPA = "tile.png";
 var VACA = "vaca.png";
 var CERDO = "cerdo.png";
 var POLLO = "pollo.png";
+
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 
-var fondo = new Image();
-fondo.src = MAPA;
-fondo.addEventListener("load", dibujarFondo);
+var fondo = {url:MAPA, cargada:false};
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
 
-var vaca = new Image();
-vaca.src = VACA;
-vaca.addEventListener("load", dibujarVacas);
+var vaca = {url:VACA, cargada:false};
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
 
-var cerdo = new Image();
-cerdo.src = CERDO;
-cerdo.addEventListener("load", dibujarCerdos);
+var cerdo = {url:CERDO, cargada:false};
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
 
-var pollo = new Image();
-pollo.src = POLLO;
-pollo.addEventListener("load", dibujarPollos);
+var pollo = {url:POLLO, cargada:false};
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
 
-function dibujarFondo() {
-  papel.drawImage(fondo, 0, 0);
+function cargarFondo() {
+  fondo.cargada = true;
+  dibujar();
 }
 
-function dibujarVacas() {
-  papel.drawImage(vaca, 10, 10);
+function cargarVacas() {
+  vaca.cargada = true;
+  dibujar();
 }
 
-function dibujarCerdos() {
-  papel.drawImage(cerdo, 100, 300);
+function cargarCerdos() {
+  cerdo.cargada = true;
+  dibujar();
 }
 
-function dibujarPollos() {
-  papel.drawImage(pollo, 300, 150);
+function cargarPollos() {
+  pollo.cargada = true;
+  dibujar();
+}
+
+function dibujar() {
+  if(fondo.cargada) {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  if(vaca.cargada) {
+    for(var i = 0 ; i < 10 ; i++) {
+      var x = aleatorio(0,420);
+      var y = aleatorio(0,420);
+      papel.drawImage(vaca.imagen, x,y);
+    }
+  }
+  if(cerdo.cargada) {
+    papel.drawImage(cerdo.imagen, 100,100);
+  }
+  if(pollo.cargada) {
+    papel.drawImage(pollo.imagen, 250,250);
+  }
 }
 
 function aleatorio(min, max) {
