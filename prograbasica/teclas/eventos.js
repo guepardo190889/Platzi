8bdc363b17dae51829756d9cfd94b5ac58a5f447
Seth Karim Luis Martínez <sluis@facturable.com>
2018-04-24 16:18:46 -0500

    ifs comentados para usar en su lugar un switch

--- prograbasica/teclas/eventos.js
+++ prograbasica/teclas/eventos.js
@@ -1,11 +1,26 @@
+document.addEventListener("keyup", dibujarTeclado);
 
 var teclas = {UP:38,DOWN:40,LEFT:37,RIGTH:39};
 
-document.addEventListener("keyup", dibujarTeclado);
-
 function dibujarTeclado(event) {
-  if(teclas.UP == event.keyCode) {
-    console.log("TECLA ARRIBA");  
+  /*if(teclas.UP == event.keyCode) {
+    console.log("TECLA ARRIBA");
+  }
+  else if(teclas.DOWN == event.keyCode) {
+    console.log("TECLA ABAJO");
+  }
+  else if(teclas.LEFT == event.keyCode) {
+    console.log("TECLA IZQUIERDA");
+  }
+  else if(teclas.RIGTH == event.keyCode) {
+    console.log("TECLA DERECHA");
+  }*/
+
+  switch(event.keyCode) {
+    case teclas.UP : console.log("TECLA ARRIBA"); break;
+    case teclas.DOWN : console.log("TECLA ABAJO"); break;
+    case teclas.LEFT : console.log("TECLA IZQUIERDA"); break;
+    case teclas.RIGTH : console.log("TECLA DERECHA"); break;
   }
 
 }
